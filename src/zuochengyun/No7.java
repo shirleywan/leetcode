0c68b406e3a57ed5ac18f4a989bd27185826ecbd
shirleywan <shirleyti1949@163.com>
2018-07-30 22:04:11 +0800

    生成窗口最大值数组

--- src/zuochengyun/No7.java
+++ src/zuochengyun/No7.java
@@ -4,34 +4,56 @@ import java.util.*;
 
 /**
  * 生成窗口最大值数组
- */
+ * 这里注意，LinkedList类就是双向链表，有First和Last两个方向；
+  */
 class Solution7{
     public int[] windowMaxArray(int[] nums , int n){
-        if( n <=0 || nums.length ==0){return null;}
+        if( n <= 0 || nums.length ==0){return null;}
         int[] array = new int[nums.length-n+1];
-        Queue<Integer> queue = new LinkedList<Integer>();
-        queue.offer(nums[0]);
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+        queue.offer(0);
         int i = 0;
         int count = 0;
         n = Integer.min(nums.length,n);
         for( i=1; i < n ; i++){
-            if(nums[i]>queue.peek()){queue.offer(nums[i]);}
-            else{queue.offer(queue.peek());}
-            queue.poll();
+            if(nums[i]<nums[queue.peekLast()]){queue.offer(i);}
+            else{
+            	while(!queue.isEmpty()){
+            		queue.poll();
+            	}
+            	queue.offer(i);
+            }
         }
-        array[count] = queue.peek();
-        for ( ; i<nums.length ; i++){
-            if(nums[i]>queue.peek()){queue.offer(nums[i]);}
-            else{queue.offer(queue.peek());}
-            array[count++]=queue.poll();
+        array[count] = nums[queue.peek()];
+        for ( i = n; i<nums.length ; i++){
+            if(queue.peekFirst() + n <= i ){queue.pollFirst();}
+//            if(queue.isEmpty()){queue.offer(i);continue;}
+            if(nums[i] <= nums[queue.peekFirst()]){
+            	if(nums[i] <= nums[queue.peekLast()]){queue.offer(i);}
+                else{
+                	while(nums[i]>nums[queue.peekLast()]){
+                		queue.pollLast();
+                	}
+                	queue.offer(i);
+                }
+            }
+            else{
+            	while(!queue.isEmpty()){
+            		queue.pollFirst();
+            	}
+            	queue.offerFirst(i);
+            }
+            count++;
+            array[count] =nums[queue.peek()];
         }
         return array;
     }
 }
 public class No7 {
     public static void main(String[] args){
-        int[] nums = {4,3,5,4,3,3,6,7};
+        int[] nums = {4,3,5,2,4,3,6,7};
         int n = 3;
-
+        int[] array = new Solution7().windowMaxArray(nums, n);
+        System.out.println(Arrays.toString(array));
     }
 }
