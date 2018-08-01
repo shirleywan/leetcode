b294c102d5b3a463490c7964b1b241399915a1e0
shirleywan <shirleyti1949@163.com>
2018-08-01 19:36:33 +0800

    双向链表节点定义

---
+++ src/zuochengyun/util/DoubleNode.java
@@ -0,0 +1,13 @@
+package zuochengyun.util;
+
+/**
+ * 双向链表节点
+ */
+public class DoubleNode {
+    public int value ;
+    public DoubleNode next ;
+    public DoubleNode last ;
+    public DoubleNode (int value) {
+        this.value = value;
+    }
+}
