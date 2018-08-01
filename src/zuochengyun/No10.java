a6f31f315d9681655345ea77aa64713f087b412e
shirleywan <shirleyti1949@163.com>
2018-08-01 18:34:17 +0800

    最大值减去最小值小于等于num的子数组数量

---
+++ src/zuochengyun/No10.java
@@ -0,0 +1,15 @@
+package zuochengyun;
+import java.util.*;
+/**
+ * 最大值减去最小值小于等于num的子数组数量
+ */
+class Solution10{
+    public int countArrayNums(int[] nums{
+        return 0;
+    }
+}
+public class No10 {
+    public static void main(String[] args){
+
+    }
+}
