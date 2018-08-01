fdae0b925f83f60cf5a7adf5452a16339a598db9
shirleywan <shirleyti1949@163.com>
2018-08-01 19:11:12 +0800

    树节点定义

---
+++ src/zuochengyun/util/TreeNode.java
@@ -0,0 +1,14 @@
+package zuochengyun.util;
+
+/**
+ * TreeNode defination
+ */
+public class TreeNode {
+    public int value;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int data){
+        this.value = data;
+    }
+}
