43443f504627b707fe7eda61461621642eb058f6
shirleywan <shirleyti1949@163.com>
2018-08-01 19:11:39 +0800

    链表节点定义

---
+++ src/zuochengyun/util/Node.java
@@ -0,0 +1,12 @@
+package zuochengyun.util;
+
+/**
+ * Node defination
+ */
+public class Node {
+    public int value ;
+    public Node next ;
+    public Node(int data){
+        this.value = data;
+    }
+}
