f3f352d839712b411401442120a8268498e27aa0
shirleywan <shirleyti1949@163.com>
2018-08-02 22:56:00 +0800

    easy 反转部分单向链表

--- src/zuochengyun/No15.java
+++ src/zuochengyun/No15.java
@@ -4,31 +4,35 @@ import zuochengyun.util.Node;
 
 /**
  * 反转部分单向链表
- */
+  */
 class Solution15{
     /**
-     *反转部分单向链表
-     */
+     * 反转部分单向链表
+      */
     public Node reverseSingleBetween(Node root ,int from , int to){
         if (root == null || root.next == null || from >= to){return root;}
         Node first = root ;
-        while(first.next.value != from ||first.next!=null){
+        while(first.next.value != from && first.next!=null){
             first = first.next;
         }
+        if (first.next.value == from && first.next.next==null){return root;}
         Node begin = first;
         first = first.next;
-        begin.next = null;
+//        begin.next = null;
         Node p = first.next;
-        while(p.value != to){
+        while(p!=null && p.value != to){ //to可以大于最后一个元素，如果大于，直接反转全部
             first.next = p.next;
-            p.next = first;
+            p.next = begin.next;
             begin.next = p;
             p = first.next;
+            
+//            first = begin.next;
         }
-        first.next = p.next;
-        p.next = first;
-        begin.next = p;
-
+        if(p!=null){
+        	first.next = p.next;
+            p.next = begin.next;
+            begin.next = p;
+        }
         return root ;
     }
 }
@@ -40,7 +44,7 @@ public class No15 {
         Node n3 = new Node(4);
         Node n4 = new Node(5);
         root.next = n1 ;n1.next = n2 ; n2.next = n3; n3.next = n4 ;
-        Node result = new Solution15().reverseSingleBetween(root , 2 , 4);
+        Node result = new Solution15().reverseSingleBetween(root , 2 , 6);
         while(result != null){
             System.out.println(result.value);
             result = result.next ;
