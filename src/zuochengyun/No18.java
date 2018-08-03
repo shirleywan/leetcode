5aa942fdea2d1a98d3a5a98b5822962462cd8e3f
shirleywan <shirleyti1949@163.com>
2018-08-03 22:12:22 +0800

    easy

---
+++ src/zuochengyun/No18.java
@@ -0,0 +1,51 @@
+package zuochengyun;
+
+import java.util.Arrays;
+
+import zuochengyun.util.Node;
+
+class Solution18{
+	public Node sort(Node head){
+		if(head == null || head.next == null){return head;}
+		Node p = head;
+		int count = 0;
+		while(p!=null){
+			count ++ ;
+			p = p.next ;
+		}
+		int[] nums = new int[count];
+		p = head;
+		count = 0;
+		while( p != null){
+			nums[count] = p.value;
+			count ++;
+			p = p.next;
+		}
+		Arrays.sort(nums);
+		p = head;
+		count = 0;
+		while(p!=null){
+			p.value = nums[count];
+			p = p.next ; 
+			count ++ ;
+		}
+		return head;
+	}
+}
+
+public class No18 {
+	public static void main(String[] args){
+        Node root = new Node(1);
+        Node n1 = new Node(2);
+        Node n2 = new Node(5);
+        Node n3 = new Node(3);
+        Node n4 = new Node(1);
+        root.next = n1 ;n1.next = n2 ; n2.next = n3; n3.next = n4 ;
+        Node result = new Solution18().sort(root);
+        while(result != null){
+            System.out.println(result.value);
+            result = result.next ;
+        }
+    }
+
+}
