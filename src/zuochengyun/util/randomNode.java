c6e315d0f715f4b191977fb622d89a0b69e887cd
shirleywan <shirleyti1949@163.com>
2018-08-04 21:22:07 +0800

    randomNode

---
+++ src/zuochengyun/util/randomNode.java
@@ -0,0 +1,14 @@
+package zuochengyun.util;
+
+/**
+ * randomNode :two pointer,point to next and other node
+ */
+public class randomNode {
+    public int value ;
+    public randomNode next ;
+    public randomNode rand;
+    
+    public randomNode(int data){
+        this.value = data;
+    }
+}
