06ae9305e51793fec16cba72f6ecebab9de8436b
shirleywan <shirleyti1949@163.com>
2018-08-16 18:57:20 +0800

    evaluate-reverse-polish-notation

---
+++ src/essential/No2.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import java.util.*;
+
+/**
+ * evaluate-reverse-polish-notation
+ * Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+ * Valid operators are+,-,*,/. Each operand may be an integer or another expression.
+ * Some examples:
+ * ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+ * ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+ */
+class Solution2{
+    public int evalRPN(String[] tokens) {
+        Stack<Character> stack = new Stack<Character>();
+        
+        return 0;
+    }
+}
+public class No2 {
+    public static void main(String[] args){
+
+    }
+}
