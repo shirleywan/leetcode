18e54e6193edca5cb8b1fcd7b15d27cc54d6dbca
shirleywan <shirleyti1949@163.com>
2018-08-27 09:56:12 +0800

    binary-tree-postorder-traversal

--- src/leetcode/No6.java
+++ src/leetcode/No6.java
@@ -1,7 +1,17 @@
 package leetcode;
 
 /**
- * insertion-sort-list
+ * binary-tree-postorder-traversal
+ * Given a binary tree, return the postorder traversal of its nodes' values.
+ * For example:
+ * Given binary tree{1,#,2,3},
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ * return[3,2,1].
+ * Note: Recursive solution is trivial, could you do it iteratively?
  */
 class Solution{
 
