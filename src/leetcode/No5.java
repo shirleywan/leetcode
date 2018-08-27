e4aec74e1bf1fc965bedbafefd57fddbd69f8b08
shirleywan <shirleyti1949@163.com>
2018-08-27 09:49:19 +0800

    insertion-sort-list

--- src/leetcode/No5.java
+++ src/leetcode/No5.java
@@ -1,7 +1,6 @@
 package leetcode;
 
 import leetcode.util.ListNode;
-import test.Link;
 
 import java.util.*;
 /**
@@ -13,38 +12,38 @@ class Solution5{
         if(head == null || head.next == null){return head;}
         ListNode first = new ListNode(0);
         first.next = head;
-        ListNode p = head.next;
-        ListNode q = first.next;
+        ListNode p = head;
+        ListNode q = first;
         ListNode last = p.next;
-        while(p != null){
-            while(q != p) {
-                last = p.next;
-                while (p.val > q.next.val) {
-                    q = q.next;
-                    if ( q.next == p ){ break; }
-                }
-
-                p.next = q.next.next;
-                q.next = p;
-                p = last;
-                q = first;
-
+        while(p.next != null){
+            q = first;
+            last = p.next.next ;
+            while( p.next.val > q.next.val && q != p) {
+                q = q.next;
             }
-            p = p.next;
+            if(q == p){
+                p = p.next;
+            }
+            else{
+                p.next.next = q.next;
+                q.next = p.next;
+                p.next = last;
+            }
+//            System.out.println("p:" + p.val);
         }
         return first.next;
     }
 }
 public class No5 {
     public static void main(String[] args){
-        ListNode n1 = new ListNode(3);
-        ListNode n2 = new ListNode(4);
-        ListNode n3 = new ListNode(2);
+        ListNode n1 = new ListNode(4);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(1);
         ListNode n4 = new ListNode(5);
-        ListNode n5 = new ListNode(1);
+        ListNode n5 = new ListNode(2);
         n1.next = n2 ;n2.next = n3; n3.next = n4; n4.next = n5;
         ListNode result = new Solution5().insertionSortList(n1);
-        while (result!= null){
+        while (result != null){
             System.out.println(result.val);
             result = result.next;
         }
---
+++ src/leetcode/No6.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+/**
+ * insertion-sort-list
+ */
+class Solution{
+
+}
+
+public class No6 {
+    public static void main(String[] args){
+
+    }
+}
