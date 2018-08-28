52ef1bf968cbd30b31ca984cde2360d816299018
shirleywan <shirleyti1949@163.com>
2018-08-28 22:30:08 +0800

    word-break-ii

---
+++ src/leetcode/No11.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * word-break-ii
+ * Given a string s and a dictionary of words dict, add spaces in s to construct a sentence where each word is a valid dictionary word.
+ * Return all such possible sentences.
+ * For example, given
+ * s ="catsanddog",
+ * dict =["cat", "cats", "and", "sand", "dog"].
+ * A solution is["cats and dog", "cat sand dog"].
+ */
+import java.util.*;
+class Solution{
+    public ArrayList<String> wordBreak(String s, Set<String> dict) {
+        return null;
+    }
+}
+public class No11 {
+    public static void main(String[] args){
+        String s ="catsanddog";
+        Set<String> dict = new HashSet<String>();
+        dict.add("cat");dict.add("cats");dict.add("and");dict.add("sand");dict.add("dog");
+
+    }
+}
