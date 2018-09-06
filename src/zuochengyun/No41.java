3bbc94d05f2ebb70d2251571df72acb7eb29b528
shirleywan <shirleyti1949@163.com>
2018-09-06 14:52:37 +0800

    mid 判断t1树是否包含t2树全部的拓扑结构

---
+++ src/zuochengyun/No41.java
@@ -0,0 +1,64 @@
+package zuochengyun;
+
+import sun.reflect.generics.tree.Tree;
+import zuochengyun.util.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 判断t1树是否包含t2树全部的拓扑结构
+ * 时间复杂度O(M)
+ */
+
+class Solution41 {
+    public boolean contains(TreeNode root , TreeNode head){
+        if(root == null || head == null) {return false;}
+        TreeNode node =null;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            node = queue.poll();
+            if(node.left != null){queue.offer(node.left);}
+            if(node.right != null){queue.offer(node.right);}
+            if(node.value == head.value){break;}
+        }
+        return check(node,head);
+    }
+    public boolean check(TreeNode root , TreeNode head){
+        if(root == null && head == null){return true;}
+        boolean flag = false;
+        if(root != null && head!= null){
+            if(root.value == head.value ){
+                flag = true;
+            }
+            else flag = false;
+        }
+        if((root == null && head != null) || (root != null && head == null)){flag = false;}
+        return flag && check(root.left , head.left) && check(root.right ,head.right);
+    }
+}
+
+public class No41 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode n1 = new TreeNode(3);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(5);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(6);
+
+        root.left = n1; root.right = n3;
+        n1.right = n2;
+        n3.left = n4; n3.right = n5;
+
+        TreeNode head = new TreeNode(5);
+        TreeNode n6 = new TreeNode(4);
+        TreeNode n7 = new TreeNode(6);
+        head.left = n6;//head.right = n7;
+        
+        Solution41 solution = new Solution41();
+        System.out.println(solution.contains(root, head));
+
+    }
+}
