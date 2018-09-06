01230e36354c4d9ecc5dccbf459a3b31153b21bb
shirleywan <shirleyti1949@163.com>
2018-09-06 09:09:00 +0800

    找到二叉树中符合搜索二叉树条件的最大拓扑结构 -- 未完成

---
+++ src/zuochengyun/No38.java
@@ -0,0 +1,31 @@
+package zuochengyun;
+
+import zuochengyun.util.TreeNode;
+
+/**
+ * 找到二叉树中符合搜索二叉树条件的最大拓扑结构 -- 未完成
+ * 时间复杂度O(N^2);
+ */
+
+class Solution38 {
+
+}
+
+public class No38 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode n1 = new TreeNode(3);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(5);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(6);
+
+        root.left = n1; root.right = n3;
+        n1.right = n2;
+        n3.left = n4; n3.right = n5;
+        
+        Solution38 solution = new Solution38();
+
+
+    }
+}
