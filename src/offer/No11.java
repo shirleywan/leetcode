dd5843219de0ca07c55d701db392427040e82307
shirleywan <shirleyti1949@163.com>
2018-10-08 15:35:17 +0800

    二进制数中1的个数

---
+++ src/offer/No11.java
@@ -0,0 +1,18 @@
+package offer;
+
+/**
+ * 二进制数中1的个数
+ * 输入一个整数，输出该数二进制表示中 1 的个数。其中负数用补码表示。
+ */
+import java.util.*;
+class Solution11 {
+    public int NumberOf1(int n) {
+
+        return 0;
+    }
+}
+public class No11 {
+    public static void main(String[] args){
+        System.out.println();
+    }
+}
