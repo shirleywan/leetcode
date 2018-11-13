7116ccb7e9f2f8b477bafef45b0e55a4de3d1a36
shirleywan <shirleyti1949@163.com>
2018-11-13 19:25:34 +0800

    556. Next Greater Element III

---
+++ src/Stack/No556NextGreaterElementIII.java
@@ -0,0 +1,23 @@
+package Stack;
+
+/**
+ * 556. Next Greater Element III
+ * Given a positive 32-bit integer n, you need to find the smallest 32-bit integer which has exactly the same digits existing in the integer n and is greater in value than n. If no such positive 32-bit integer exists, you need to return -1.
+ *
+ * Example 1:
+ *      Input: 12
+ *      Output: 21
+ * Example 2:
+ *      Input: 21
+ *      Output: -1
+ */
+class Solution556 {
+    public int nextGreaterElement(int n) {
+        return 0;
+    }
+}
+public class No556NextGreaterElementIII {
+    public static void main(String[] args){
+        long num = 21;
+    }
+}
