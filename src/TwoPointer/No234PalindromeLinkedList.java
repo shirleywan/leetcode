0f4fed18c7bdaa404456ae902962b9acfdb4021b
shirleywan <shirleyti1949@163.com>
2018-11-27 10:48:12 +0800

    234. Palindrome Linked List

--- src/TwoPointer/No234PalindromeLinkedList.java
+++ src/TwoPointer/No234PalindromeLinkedList.java
@@ -6,7 +6,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 
+ * 234. Palindrome Linked List
+ * Given a singly linked list, determine if it is a palindrome.
+ * Example 1:
+ *      Input: 1->2
+ *      Output: false
+ * Example 2:
+ *      Input: 1->2->2->1
+ *      Output: true
+ * Follow up:
+ * Could you do it in O(n) time and O(1) space?
  */
 class Solution234 {
     /**
