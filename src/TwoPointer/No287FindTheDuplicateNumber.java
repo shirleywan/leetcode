a326d3766b53d27521a5ccf5048b024c57d21f5a
shirleywan <shirleyti1949@163.com>
2018-12-13 21:33:45 +0800

    No 287

--- src/TwoPointer/No287FindTheDuplicateNumber.java
+++ src/TwoPointer/No287FindTheDuplicateNumber.java
@@ -15,8 +15,45 @@ package TwoPointer;
  *      Your runtime complexity should be less than O(n2).
  *      There is only one duplicate number in the array, but it could be repeated more than once.
  */
+class Solution287 {
+    /**
+     * Runtime: 1 ms, faster than 64.54% of Java online submissions for Find the Duplicate Number.
+     * 将遍历过的点设置为0，如果重新遍历了这个点，则返回这个值；
+     */
+    public int findDuplicate(int[] nums) {
+        if(nums == null || nums.length == 0) return -1;
+        int i = 0 ;
+        int temp = 0;
+        while(nums[i] != 0){
+            temp = nums[i];
+            nums[i] = 0;
+            i = temp;
+        }
+        return i;
+    }
+    /**
+     * good method：
+     * Runtime: 0 ms, faster than 100.00% of Java online submissions for Find the Duplicate Number.
+     */
+    public int findDuplicate1(int[] nums) {
+        int n = nums.length;
+        int slow = n;
+        int fast = n;
+        do{
+            slow = nums[slow-1];
+            fast = nums[nums[fast-1]-1];
+        }while(slow != fast);
+        slow = n;
+        while(slow != fast){
+            slow = nums[slow-1];
+            fast = nums[fast-1];
+        }
+        return slow;
+    }
+}
 public class No287FindTheDuplicateNumber {
     public static void main(String[] args){
-        int[] nums = {1,3,4,2,2};
+        int[] nums = {3,1,3,4,2};
+        System.out.println(new Solution287().findDuplicate(nums));
     }
 }
